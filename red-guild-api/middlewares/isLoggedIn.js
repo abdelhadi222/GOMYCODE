fb6ff5e92c0a215a55cf999045d200efbb0765d2
Hamza Chebbah <hamzachebbah9999@gmail.com>
2023-03-04 09:23:25 +0100

    code refactor

--- red-guild-api/middlewares/isLoggedIn.js
+++ red-guild-api/middlewares/isLoggedIn.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken"
+import { User } from "../schemas/user.js"
 
 export const isLoggedIn = (req, res, next) => {
   const { token } = req.headers
@@ -7,13 +8,15 @@ export const isLoggedIn = (req, res, next) => {
       message: "unauthorized"
     })
   } else {
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
       if (err) {
         res.status(401).json({
           message: "unauthorized"
         })
       } else {
-        req.validateToken = decoded
+        const FoundUser = await User.findById(decoded.id)
+        const { password, ...rest } = FoundUser._doc
+        req.user = rest
         next()
       }
     })
--- red-guild-api/routes/user.js
+++ red-guild-api/routes/user.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import { addUser, deleteUser, getAllUser, login } from '../controllers/user.js'
 import { isLoggedIn } from '../middlewares/isLoggedIn.js'
-import { User } from '../schemas/user.js'
 
 const router = express.Router()
 
@@ -13,8 +12,6 @@ router.post("/signup", addUser)
 router.delete("/:id", deleteUser)
 // router.get("/:id", getUserById)
 router.get("/checktoken", isLoggedIn, async (req, res) => {
-  const FoundUser = await User.findById(req.validateToken.id)
-  const { password, ...rest } = FoundUser._doc
-  res.json(rest)
+  res.json(req.user)
 })
 export default router
